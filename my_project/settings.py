136494541cb15cc8682015be382c10bf47f700f6
louisae452 <louisae452@msn.com>
2026-05-01 11:15:04 +0100

    Fix spelling

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=!4k@nm6^2td!0#1m2nl)f#k^n$3s8(@y7zouu!s4*g=syuil@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokyapp.com', '127.0.0.1',]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
